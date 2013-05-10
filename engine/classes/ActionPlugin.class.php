102c65a4921b5f6632505216f08c3a8c646a73e8
aVadim <vadim483@gmail.com>
2013-05-10 21:42:52 +0400

    реформат кода в соответствии со стилем кодирования

--- engine/classes/ActionPlugin.class.php
+++ engine/classes/ActionPlugin.class.php
@@ -21,59 +21,66 @@ require_once('Action.class.php');
  * От этого класса необходимо наследовать экшены плагина, эот позволит корректно определять текущий шаблон плагина для рендеринга экшена
  *
  * @package engine
- * @since 1.0
+ * @since   1.0
  */
 abstract class ActionPlugin extends Action {
-	/**
-	 * Полный серверный путь до текущего шаблона плагина
-	 *
-	 * @var string|null
-	 */
-	protected $sTemplatePathPlugin=null;
+    /**
+     * Полный серверный путь до текущего шаблона плагина
+     *
+     * @var string|null
+     */
+    protected $sTemplatePathPlugin = null;
 
-	/**
-	 * Возвращает путь к текущему шаблону плагина
-	 *
-	 * @return string
-	 */
-	public function getTemplatePathPlugin() {
-		if(is_null($this->sTemplatePathPlugin)) {
-			preg_match('/^Plugin([\w]+)_Action([\w]+)$/i',$this->GetActionClass(),$aMatches);
-			/**
-			 * Проверяем в списке шаблонов
-			 */
-			$aMatches[1]=strtolower($aMatches[1]);
-			$aPaths=glob(Config::Get('path.root.server').'/plugins/'.$aMatches[1].'/templates/skin/*/actions/Action'.ucfirst($aMatches[2]),GLOB_ONLYDIR);
-			$sTemplateName=($aPaths and in_array(
-				Config::Get('view.skin'),
-				array_map(
-					create_function(
-						'$sPath',
-						'preg_match("/skin\/([\w\-]+)\/actions/i",$sPath,$aMatches); return $aMatches[1];'
-					),
-					$aPaths
-				)
-			))
-				? Config::Get('view.skin')
-				: 'default';
+    /**
+     * Возвращает путь к текущему шаблону плагина
+     *
+     * @return string
+     */
+    public function getTemplatePathPlugin() {
+        if (is_null($this->sTemplatePathPlugin)) {
+            preg_match('/^Plugin([\w]+)_Action([\w]+)$/i', $this->GetActionClass(), $aMatches);
+            /**
+             * Проверяем в списке шаблонов
+             */
+            $aMatches[1] = strtolower($aMatches[1]);
+            $aPaths = glob(
+                Config::Get('path.root.server') . '/plugins/' . $aMatches[1] . '/templates/skin/*/actions/Action'
+                    . ucfirst($aMatches[2]), GLOB_ONLYDIR
+            );
+            $sTemplateName = ($aPaths && in_array(
+                Config::Get('view.skin'),
+                array_map(
+                    create_function(
+                        '$sPath',
+                        'preg_match("/skin\/([\w\-]+)\/actions/i",$sPath,$aMatches); return $aMatches[1];'
+                    ),
+                    $aPaths
+                )
+            ))
+                ? Config::Get('view.skin')
+                : 'default';
 
-			$sDir=Config::Get('path.root.server')."/plugins/{$aMatches[1]}/templates/skin/{$sTemplateName}/";
-			$this->sTemplatePathPlugin = is_dir($sDir) ? $sDir : null;
-		}
+            $sDir = Config::Get('path.root.server') . "/plugins/{$aMatches[1]}/templates/skin/{$sTemplateName}/";
+            $this->sTemplatePathPlugin = is_dir($sDir) ? $sDir : null;
+        }
 
-		return $this->sTemplatePathPlugin;
-	}
+        return $this->sTemplatePathPlugin;
+    }
 
-	/**
-	 * Установить значение пути к директории шаблона плагина
-	 *
-	 * @param  string $sTemplatePath	Полный серверный путь до каталога с шаблоном
-	 * @return bool
-	 */
-	public function setTemplatePathPlugin($sTemplatePath) {
-		if(!is_dir($sTemplatePath)) return false;
-		$this->sTemplatePathPlugin = $sTemplatePath;
-	}
+    /**
+     * Установить значение пути к директории шаблона плагина
+     *
+     * @param  string $sTemplatePath    Полный серверный путь до каталога с шаблоном
+     *
+     * @return bool
+     */
+    public function setTemplatePathPlugin($sTemplatePath) {
+        if (!is_dir($sTemplatePath)) {
+            return false;
+        }
+        $this->sTemplatePathPlugin = $sTemplatePath;
+    }
 
 }
-?>
+
+// EOF
--- engine/classes/ManyToManyRelation.class.php
+++ engine/classes/ManyToManyRelation.class.php
@@ -27,70 +27,76 @@
  * наличии настроенной MANY_TO_MANY связи 'tags'
  *
  * @package engine.orm
- * @since 1.0
+ * @since   1.0
  */
 class LS_ManyToManyRelation extends LsObject {
-	/**
-	 * Список объектов связи
-	 *
-	 * @var array
-	 */
-	protected $_aCollection = array();
-	/**
-	 * Флаг обновления списка объектов связи
-	 *
-	 * @var bool
-	 */
-	protected $bUpdated = false;
+    /**
+     * Список объектов связи
+     *
+     * @var array
+     */
+    protected $_aCollection = array();
+    /**
+     * Флаг обновления списка объектов связи
+     *
+     * @var bool
+     */
+    protected $bUpdated = false;
 
-	/**
-	 * Устанавливает список объектов
-	 *
-	 * @param $aCollection	Список объектов связи
-	 */
-	public function __construct($aCollection) {
-		$this->_aCollection = $aCollection;
-	}
-	/**
-	 * Добавление объекта в список
-	 *
-	 * @param Entity $oEntity
-	 */
-	public function add($oEntity) {
-		$this->bUpdated = true;
-		$this->_aCollection[$oEntity->_getPrimaryKeyValue()] = $oEntity;
-	}
-	/**
-	 * Удаление объекта из списка по его id или массиву id
-	 *
-	 * @param int|array $iId
-	 */
-	public function delete($iId) {
-		$this->bUpdated = true;
-		if (is_array($iId)) {
-			foreach ($iId as $id) {
-				if (isset($this->_aCollection[$id])) {
-					unset($this->_aCollection[$id]);
-				}
-			}
-		} elseif (isset($this->_aCollection[$iId])) {
-			unset($this->_aCollection[$iId]);
-		}
-	}
-	/**
-	 * Возвращает список объектов связи
-	 *
-	 * @return array
-	 */
-	public function getCollection() {
-		return $this->_aCollection;
-	}
-	/**
-	 * Проверка списка на обновление
-	 *
-	 * @return bool
-	 */
-	public function isUpdated() {
-		return $this->bUpdated;
-	}
+    /**
+     * Устанавливает список объектов
+     *
+     * @param $aCollection    Список объектов связи
+     */
+    public function __construct($aCollection) {
+        $this->_aCollection = $aCollection;
+    }
+
+    /**
+     * Добавление объекта в список
+     *
+     * @param Entity $oEntity
+     */
+    public function add($oEntity) {
+        $this->bUpdated = true;
+        $this->_aCollection[$oEntity->_getPrimaryKeyValue()] = $oEntity;
+    }
+
+    /**
+     * Удаление объекта из списка по его id или массиву id
+     *
+     * @param int|array $iId
+     */
+    public function delete($iId) {
+        $this->bUpdated = true;
+        if (is_array($iId)) {
+            foreach ($iId as $id) {
+                if (isset($this->_aCollection[$id])) {
+                    unset($this->_aCollection[$id]);
+                }
+            }
+        } elseif (isset($this->_aCollection[$iId])) {
+            unset($this->_aCollection[$iId]);
+        }
+    }
+
+    /**
+     * Возвращает список объектов связи
+     *
+     * @return array
+     */
+    public function getCollection() {
+        return $this->_aCollection;
+    }
+
+    /**
+     * Проверка списка на обновление
+     *
+     * @return bool
+     */
+    public function isUpdated() {
+        return $this->bUpdated;
+    }
 }
+
+// EOF
