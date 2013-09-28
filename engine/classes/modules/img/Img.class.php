1c656859ce084d0ce9d9aba141b3f1260f0e8399
aVadim <vadim483@gmail.com>
2013-09-28 20:42:28 +0400

    added method ModuleImg::MimeType(<file>)

--- engine/classes/modules/img/Img.class.php
+++ engine/classes/modules/img/Img.class.php
@@ -24,6 +24,27 @@ class ModuleImg extends Module {
     protected $aAvailableDrivers = array();
     protected $aParams = array();
 
+    static protected $aMimeTypeSignatures
+        = array(
+            'image/gif'  => array(
+                array('offset' => 0, 'signature' => 'GIF87a'),
+                array('offset' => 0, 'signature' => 'GIF89a'),
+            ),
+            'image/jpeg' => array(
+                array('offset' => 0, 'signature' => "\xFF\xD8\xFF"),
+            ),
+            'image/png'  => array(
+                array('offset' => 0, 'signature' => "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"),
+            ),
+            'image/tiff' => array(
+                array('offset' => 0, 'signature' => "\x49\x20\x49"),
+                array('offset' => 0, 'signature' => "\x49\x49\x2A\x00"),
+                array('offset' => 0, 'signature' => "\x4D\x4D\x00\x2A"),
+                array('offset' => 0, 'signature' => "\x4D\x4D\x00\x2B"),
+            ),
+        );
+    static protected $nMimeTypeSignaturesMax = 0;
+
     public function Init() {
 
         $this->aAvailableDrivers = $this->GetDriversInfo();
@@ -592,6 +613,46 @@ class ModuleImg extends Module {
         $sText .= $sAlt . ' />';
 
         return $sText;
+    }
+
+    static public function MimeType($sFile) {
+
+        $sBuffer = '';
+        if (!self::$nMimeTypeSignaturesMax) {
+            foreach (self::$aMimeTypeSignatures as $sMimeType => $aSignsCollect) {
+                if (isset($aSignsCollect['offset']) || isset($aSignsCollect['offset'])) {
+                    $aSignsCollect = array($aSignsCollect);
+                }
+                foreach ($aSignsCollect as $nIdx => $aSign) {
+                    if (is_string($aSign)) {
+                        $nOffset = 0;
+                        $sSignature = $aSign;
+                    } else {
+                        $nOffset = isset($aSign['offset']) ? intval($aSign['offset']) : 0;
+                        $sSignature = isset($aSign['signature']) ? intval($aSign['signature']) : '';
+                    }
+                    $nLen = $nOffset + strlen($sSignature);
+                    if ($nLen > self::$nMimeTypeSignaturesMax) {
+                        self::$nMimeTypeSignaturesMax = $nLen;
+                    }
+                    self::$aMimeTypeSignatures[$sMimeType][$nIdx] = array(
+                        'offset' => $nOffset,
+                        'signature' => $sSignature,
+                    );
+                }
+            }
+        }
+        if ($hFile = fopen($sFile, 'r')) {
+            $sBuffer = fgets($hFile, self::$nMimeTypeSignaturesMax);
+            fclose($hFile);
+            foreach (self::$aMimeTypeSignatures as $sMimeType => $aSignsCollect) {
+                foreach ($aSignsCollect as $aSign) {
+                    if (substr($sBuffer, $aSign['offset'], strlen($aSign['signature'])) == $aSign['signature']) {
+                        return $sMimeType;
+                    }
+                }
+            }
+        }
     }
 
 }
