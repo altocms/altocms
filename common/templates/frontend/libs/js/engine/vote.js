dc6e55b9db7c553a7fa9472d0faf1c1557dd5d90
aVadim <vadim483@gmail.com>
2014-04-09 17:21:00 +0400

    js voting

--- common/templates/frontend/libs/js/engine/vote.js
+++ common/templates/frontend/libs/js/engine/vote.js
@@ -63,34 +63,34 @@ ls.vote = (function ($) {
         this.options = $.extend({}, defaults, options);
 
         $(this.options.selectors.vote).each(function () {
-            var oVote = $(this);
+            var voteSection = $(this);
 
-            var oVars = {
-                vote: oVote,
-                up: oVote.find(self.options.selectors.up),
-                down: oVote.find(self.options.selectors.down),
-                abstain: oVote.find(self.options.selectors.abstain),
-                count: oVote.find(self.options.selectors.count),
-                rating: oVote.find(self.options.selectors.rating),
-                id: oVote.data('vote-id'),
-                type: oVote.data('vote-type')
+            var voteOptions = {
+                vote: voteSection,
+                up: voteSection.find(self.options.selectors.up),
+                down: voteSection.find(self.options.selectors.down),
+                abstain: voteSection.find(self.options.selectors.abstain),
+                count: voteSection.find(self.options.selectors.count),
+                rating: voteSection.find(self.options.selectors.rating),
+                id: voteSection.data('target-id'),
+                type: voteSection.data('target-type')
             };
 
             // Плюс
-            oVars.up.on('click', function (e) {
-                self.vote(oVars.id, 1, oVars.type, oVars);
+            voteOptions.up.on('click', function (e) {
+                self.vote(voteOptions.type, voteOptions.id, 1, voteOptions);
                 e.preventDefault();
             });
 
             // Минус
-            oVars.down.on('click', function (e) {
-                self.vote(oVars.id, -1, oVars.type, oVars);
+            voteOptions.down.on('click', function (e) {
+                self.vote(voteOptions.type, voteOptions.id, -1, voteOptions);
                 e.preventDefault();
             });
 
             // Воздержаться
-            oVars.abstain.on('click', function (e) {
-                self.vote(oVars.id, 0, oVars.type, oVars);
+            voteOptions.abstain.on('click', function (e) {
+                self.vote(voteOptions.type, voteOptions.id, 0, voteOptions);
                 e.preventDefault();
             });
         });
@@ -99,77 +99,84 @@ ls.vote = (function ($) {
     /**
      * Голосование
      *
-     * @param  {Number} iTargetId ID объекта
-     * @param  {Number} iValue    Значение
-     * @param  {String} sType     Тип голосования
-     * @param  {Object} oVars     Переменные текущего голосования
+     * @param  {String} targetType
+     * @param  {Number} targetId
+     * @param  {Number} voteValue
+     * @param  {Object} voteOption
      */
-    this.vote = function (iTargetId, iValue, sType, oVars) {
-        var self = this;
+    this.vote = function (targetType, targetId, voteValue, voteOption) {
+        var target = this.options.type[targetType];
 
-        if (!this.options.type[sType]) return false;
+        if (!target) {
+            return false;
+        }
 
         var params = {
-            value: iValue
+            value: voteValue
         };
-        params[this.options.type[sType].targetName] = iTargetId;
+        params[target.targetName] = targetId;
 
-        ls.ajax(this.options.type[sType].url, params, function (result) {
-            var args = [iTargetId, iValue, sType, oVars, result];
+        ls.progressStart();
+        ls.ajax(target.url, params, function (response) {
+            ls.progressDone();
+            var args = [targetType, targetId, voteValue, voteOption, response];
             this.onVote.apply(this, args);
         }.bind(this));
+
+        return false;
     };
 
     /**
      * Коллбэк вызываемый при успешном голосовании
      *
-     * @param  {Number} iTargetId ID объекта
-     * @param  {Number} iValue    Значение
-     * @param  {String} sType     Тип голосования
-     * @param  {Object} oVars     Переменные текущего голосования
-     * @param  {Object} result    Объект возвращемый сервером
+     * @param  {String} targetType
+     * @param  {Number} targetId
+     * @param  {Number} voteValue
+     * @param  {Object} voteOptions
+     * @param  {Object} response
      */
-    this.onVote = function (iTargetId, iValue, sType, oVars, result) {
-        if (!result) {
-            ls.msg.error(null, 'System error #1001');
-        } else if (result.bStateError) {
-            ls.msg.error(null, result.sMsg);
-        } else {
-            ls.msg.notice(null, result.sMsg);
+    this.onVote = function (targetType, targetId, voteValue, voteOptions, response) {
 
-            oVars.vote
+        if (!response) {
+            ls.msg.error(null, 'System error #1001');
+        } else if (response.bStateError) {
+            ls.msg.error(null, response.sMsg);
+        } else {
+            ls.msg.notice(null, response.sMsg);
+
+            voteOptions.vote
                 .addClass(this.options.classes.voted)
                 .removeClass(this.options.classes.negative + ' ' + this.options.classes.positive + ' ' + this.options.classes.not_voted + ' ' + this.options.classes.zero);
 
-            if (iValue > 0) {
-                oVars.vote.addClass(this.options.classes.plus);
-            } else if (iValue < 0) {
-                oVars.vote.addClass(this.options.classes.minus);
-            } else if (iValue == 0) {
-                oVars.vote.addClass(this.options.classes.voted_zero);
+            if (voteValue > 0) {
+                voteOptions.vote.addClass(this.options.classes.plus);
+            } else if (voteValue < 0) {
+                voteOptions.vote.addClass(this.options.classes.minus);
+            } else if (voteValue == 0) {
+                voteOptions.vote.addClass(this.options.classes.voted_zero);
             }
 
-            if (oVars.count.length > 0 && result.iCountVote) {
-                oVars.count.text(parseInt(result.iCountVote));
+            if (voteOptions.count.length > 0 && response.iCountVote) {
+                voteOptions.count.text(parseInt(response.iCountVote));
             }
 
-            result.iRating = parseFloat(result.iRating);
+            response.iRating = parseFloat(response.iRating);
 
-            if (result.iRating > 0) {
-                oVars.vote.addClass(this.options.classes.positive);
-                oVars.rating.text('+' + result.iRating);
-            } else if (result.iRating < 0) {
-                oVars.vote.addClass(this.options.classes.negative);
-                oVars.rating.text(result.iRating);
-            } else if (result.iRating == 0) {
-                oVars.vote.addClass(this.options.classes.zero);
-                oVars.rating.text(0);
+            if (response.iRating > 0) {
+                voteOptions.vote.addClass(this.options.classes.positive);
+                voteOptions.rating.text('+' + response.iRating);
+            } else if (response.iRating < 0) {
+                voteOptions.vote.addClass(this.options.classes.negative);
+                voteOptions.rating.text(response.iRating);
+            } else if (response.iRating == 0) {
+                voteOptions.vote.addClass(this.options.classes.zero);
+                voteOptions.rating.text(0);
             }
 
-            var method = 'onVote' + ls.tools.ucfirst(sType);
+            var method = 'onVote' + ls.tools.ucfirst(targetType);
 
             if (typeof this[method] == 'function') {
-                this[method].apply(this, [iTargetId, iValue, sType, oVars, result]);
+                this[method].apply(this, [targetType, targetId, voteValue, voteOptions, response]);
             }
         }
     };
@@ -177,15 +184,19 @@ ls.vote = (function ($) {
     /**
      * Голосование за топик
      *
-     * @param  {Number} iTargetId ID объекта
-     * @param  {Number} iValue    Значение
-     * @param  {String} sType     Тип голосования
-     * @param  {Object} oVars     Переменные текущего голосования
-     * @param  {Object} result    Объект возвращемый сервером
+     * @param  {String} targetType     Тип голосования
+     * @param  {Number} targetId ID объекта
+     * @param  {Number} voteValue    Значение
+     * @param  {Object} voteOptions     Переменные текущего голосования
+     * @param  {Object} response    Объект возвращемый сервером
      */
-    this.onVoteTopic = function (iTargetId, iValue, sType, oVars, result) {
-        oVars.vote.addClass('js-tooltip-vote-topic').tooltip('enter');
+    this.onVoteTopic = function (targetType, targetId, voteValue, voteOptions, response) {
+        voteOptions.vote.addClass('js-tooltip-vote-topic').tooltip('enter');
     };
+
+    $(function(){
+        ls.vote.init();
+    });
 
     return this;
 }).call(ls.vote || {}, jQuery);
