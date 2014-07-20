09475f63bf7d2f5be6084266881e95fdc9fd3149
aVadim <vadim483@gmail.com>
2014-07-20 14:44:50 +0400

    plugin Ls - update (module Notify)

--- common/plugins/ls/classes/modules/notify/Notify.class.php
+++ common/plugins/ls/classes/modules/notify/Notify.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /*---------------------------------------------------------------------------
  * @Project: Alto CMS
  * @Project URI: http://altocms.com
@@ -21,16 +22,42 @@ class PluginLs_ModuleNotify extends PluginLs_Inherits_ModuleNotify {
     public function GetTemplatePath($sName, $xPlugin = null) {
 
         $sResult = parent::GetTemplatePath($sName, $xPlugin);
-        if (!is_dir($sResult)) {
-            $sSubDir = $this->sDir;
-            $this->sDir = 'notify';
-            $sFileName = parent::GetTemplatePath($sName, $xPlugin);
-            $this->sDir = $sSubDir;
-            $sResult = $sFileName;
-            if (!F::File_Exists($sFileName) && strpos(basename($sFileName), 'email.notify.') === 0) {
+        if (!file_exists($sResult)) {
+            if ($xPlugin) {
+                $sResult = $this->_getTemplatePathByPlugin($sName, $xPlugin, 'notify');
+            }
+            if (!file_exists($sResult) && strpos($sResult, '/notify/russian/email.')) {
+                $sLsTemplate = str_replace('/notify/russian/email.', '/notify/russian/', $sResult);
+                if (file_exists($sLsTemplate)) {
+                    $sResult = $sLsTemplate;
+                }
+            }
+        }
+        return $sResult;
+    }
+
+    protected function _getTemplatePathByPlugin($sName, $xPlugin, $sSubDir) {
+
+        $sSavedSubDir = $this->sDir;
+        $this->sDir = $sSubDir;
+        $sFileName = parent::GetTemplatePath($sName, $xPlugin);
+        $this->sDir = $sSavedSubDir;
+        $sResult = $sFileName;
+        if (!F::File_Exists($sFileName)) {
+            if (strpos(basename($sFileName), 'email.notify.') === 0) {
                 if (F::File_Exists($sFileName = str_replace('email.notify.', 'notify.', $sFileName))) {
                     $sResult = $sFileName;
                 } elseif (F::File_Exists($sFileName = str_replace('email.notify.', 'email.', $sFileName))) {
+                    $sResult = $sFileName;
+                } elseif (F::File_Exists($sFileName = str_replace('email.notify.', '', $sFileName))) {
+                    $sResult = $sFileName;
+                }
+            } elseif (strpos(basename($sFileName), 'notify.notify.') === 0) {
+                if (F::File_Exists($sFileName = str_replace('notify.notify.', 'notify.', $sFileName))) {
+                    $sResult = $sFileName;
+                } elseif (F::File_Exists($sFileName = str_replace('notify.notify.', 'email.', $sFileName))) {
+                    $sResult = $sFileName;
+                } elseif (F::File_Exists($sFileName = str_replace('notify.notify.', '', $sFileName))) {
                     $sResult = $sFileName;
                 }
             }
