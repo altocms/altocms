865ed4dc05c06513d4c77844a70ba34f225d1f1d
andreyv <andrey@gladcode.ru>
2015-02-25 08:52:10 +0300

    Исправлена ошибка в проверке имени аватара блога

--- engine/classes/modules/img/Img.class.php
+++ engine/classes/modules/img/Img.class.php
@@ -811,7 +811,7 @@ class ModuleImg extends Module {
 
         $sImageFile = '';
         $sName = basename($sFile);
-        if (preg_match('/^' . preg_quote($sPrefix) . '_([a-z0-9-]+)?_([a-z0-9\-]+)(_(male|female))?([\-0-9a-z\.]+)?(\.[a-z]+)$/i', $sName, $aMatches)) {
+        if (preg_match('/^' . preg_quote($sPrefix) . '_([a-z0-9-]+)(_[a-z0-9\-]+)?(_(male|female))?([\-0-9a-z\.]+)?(\.[a-z]+)$/i', $sName, $aMatches)) {
             $sName = $aMatches[0];
             $sSkin = $aMatches[1];
             $sType = $aMatches[2];
