e1b1f693cd7dacd5b1f4c21e0d4d21ae74c6ab92
andreyv <andrey@gladcode.ru>
2015-03-07 08:41:02 +0300

    Добавлена возможность обработки файлов ресурса при подключении их непосредственно в шаблоне, #241, #289

--- engine/classes/modules/viewer/plugs/function.asset.php
+++ engine/classes/modules/viewer/plugs/function.asset.php
@@ -60,7 +60,7 @@ function smarty_function_asset($aParams, $oSmartyTemplate) {
             if (isset($aParams['prepare'])) {
                 /** @var ModuleViewerAsset $oLocalViewerAsset */
                 $oLocalViewerAsset = new ModuleViewerAsset(Engine::getInstance());
-                $oLocalViewerAsset->AddFiles('*', array($sFile));
+                $oLocalViewerAsset->AddFiles(F::File_GetExtension($sFile, true), array($sFile));
                 $oLocalViewerAsset->Prepare();
                 $sUrl = $oLocalViewerAsset->AssetFileUrl(F::File_NormPath($sFile));
             } else {
