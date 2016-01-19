4bd4f305f27b3dc1deb153a094dfe14efd04a82b
Dmitry M <s@hitagi.ru>
2016-01-19 17:59:38 +0400

    Code style

--- common/console/ls.php
+++ common/console/ls.php
@@ -1,14 +1,14 @@
 <?php
 
 // Для эмуляции работы, т.к используется в конфиге
-$_SERVER['HTTP_HOST']='localhost';
+$_SERVER['HTTP_HOST'] = 'localhost';
 
 defined('ALTO_DIR') || define('ALTO_DIR', dirname(realpath((dirname(__FILE__)) . "/../")));
 set_include_path(get_include_path() . PATH_SEPARATOR . ALTO_DIR);
 chdir(ALTO_DIR);
 
 require_once(ALTO_DIR . '/engine/loader.php');
-require_once(dirname(__FILE__).'/lsc.php');
+require_once(dirname(__FILE__) . '/lsc.php');
 
 
 LSC::Start();
