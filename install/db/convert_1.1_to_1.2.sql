d5e46b72cba744697299d3e4108a70807dd4c613
aVadim483 <vadim483@gmail.com>
2016-03-20 20:00:29 +0300

    SQL converter to 1.2

---
+++ install/db/convert_1.1_to_1.2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE  `prefix_comment` ADD  `comment_extra` TEXT NULL DEFAULT NULL ;
+ALTER TABLE  `prefix_user` ADD  `user_extra` TEXT NULL DEFAULT NULL ;
+ALTER TABLE  `prefix_blog` ADD  `blog_extra` TEXT NULL DEFAULT NULL ;
+
