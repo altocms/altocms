cc2ec02d70951b582fe87e883326d444d57bf807
aVadim483 <vadim483@gmail.com>
2016-07-06 00:36:46 +0300

    JShrink was updated up to ver. 1.1.0

--- common/config/classes.php
+++ common/config/classes.php
@@ -20,6 +20,7 @@ return array(
         'Jevix' => '___path.dir.libs___/Jevix/jevix.class.php',
         'Qevix' => '___path.dir.libs___/php-qevix-0.4/qevix.php',
         'csstidy' => '___path.dir.libs___/CSSTidy-1.5.5/class.csstidy.php',
+        'JShrink\Minifier' => '___path.dir.libs___/JShrink-1.1.0/src/JShrink/Minifier.php',
     ),
     'prefix' => array(
         'DbSimple_' => '___path.dir.libs___/DbSimple3/lib/',
