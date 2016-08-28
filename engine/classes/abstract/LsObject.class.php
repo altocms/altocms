337c3ec9a91bcef2158e353fa06b190e09cfc912
aVadim483 <vadim483@gmail.com>
2016-08-29 01:07:57 +0300

    fix typo

--- engine/classes/abstract/LsObject.class.php
+++ engine/classes/abstract/LsObject.class.php
@@ -44,8 +44,8 @@ abstract class LsObject {
             return E::getInstance()->_CallModule($sName, $aArgs);
         } else {
             // Если подчеркивания нет, то вызов несуществующего метода
-            $oExeption = new Exception('Method "' . $sName . '" not exists in class "' . get_class($this) . '"');
-            $aStack = $oExeption->getTrace();
+            $oException = new Exception('Method "' . $sName . '" not exists in class "' . get_class($this) . '"');
+            $aStack = $oException->getTrace();
 
             if (!$aStack) {
                 $aStack = debug_backtrace();
@@ -58,15 +58,15 @@ abstract class LsObject {
                     if (preg_match('/[A-Z]\w+\_' . preg_quote($sName) . '/', $aCaller['function'])
                         || $aCaller['function'] == $sName
                     ) {
-                        $oExeption->sAdditionalInfo = 'In file ' . $aCaller['file'];
+                        $oException->sAdditionalInfo = 'In file ' . $aCaller['file'];
                         if (isset($aCaller['line'])) {
-                            $oExeption->sAdditionalInfo .= ' on line ' . $aCaller['line'];
+                            $oException->sAdditionalInfo .= ' on line ' . $aCaller['line'];
                         }
                         break;
                     }
                 }
             }
-            throw $oExeption;
+            throw $oException;
         }
     }
 
